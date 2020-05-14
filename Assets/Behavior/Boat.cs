e7af4afb50dc7d6f3f7330dbbb323ed2524f0372
Clockwork-Rat <isaiahraspet@gmail.com>
2020-05-14 10:20:01 -0700

    started movement mechanics

---
+++ Assets/Behavior/Boat.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Boat : MonoBehaviour
+{
+
+    Rigidbody rb;
+    public Canvas Canvas;
+    public Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+        //text = Canvas.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Player"))
+        {
+            text.text = "Press b to board";
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Player"))
+        {
+            text.text = "";
+        }
+    }
+}
---
+++ Assets/Behavior/Bouyant.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bouyant : MonoBehaviour
+{
+    Rigidbody rb;
+    public float upwardForce = 20;
+
+    //A Higher Number means higher drag
+    public float drag = 1;
+    public float angularDrag = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject.transform.position.y < 0)
+        {
+            rb.AddForce(new Vector3(0f, upwardForce, 0f));
+            rb.AddForce(rb.velocity * -1 * drag);
+            rb.AddTorque(rb.angularVelocity * -1 * angularDrag);
+        }
+    }
+
+    //void OnTriggerStay(Collider other)
+    //{
+    //    if (other.gameObject.tag.Equals("Water"))
+    //    {
+            
+    //    }
+    //}
+}
